80d06ebbb9413fd16c384eb6755bb45d728b2bc6
shreyasgune <sgune@protonmail.com>
2025-02-12 15:00:57 -0500

    see_sharp_game_api -

--- game-web-api-dotnet/GameBackend/obj/Debug/net8.0/GameBackend.AssemblyInfo.cs
+++ game-web-api-dotnet/GameBackend/obj/Debug/net8.0/GameBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+500502e1b22cbcb94bad5ed0b56a8dd8d8a5dedf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5a7ec45d0157c18b5372cb19eabf014c172509e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
